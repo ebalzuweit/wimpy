05b11c36b1f1dd55eb0fcdefcdd5e15e67aaa8fb
Evan Balzuweit <ebalzuweit@gmail.com>
2021-09-03 11:44:02 -0500

    logging update

--- main.py
+++ main.py
@@ -13,24 +13,33 @@ from wimpy.WindowManager import WindowManager
 PROGRAM_NAME = "wimpy"
 TRAY_ICON_PATH = os.path.join(os.getcwd(), "icon", "icon.ico")
 CONFIG_FILENAME = "config.ini"
+LOG_FILENAME = "log.txt"
+
 
 def configure():
-	logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s]: %(message)s")
-	config.load_file(CONFIG_FILENAME)
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s [%(levelname)s]: %(message)s",
+                        handlers=[
+                            logging.StreamHandler(sys.stdout),
+                            logging.FileHandler(LOG_FILENAME, encoding="utf-8")
+                        ])
+    config.load_file(CONFIG_FILENAME)
+
 
 def main():
-	configure()
+    configure()
 
-	strategy = BSPTilingStrategy()
-	window_manager = WindowManager(strategy)
-	event_handler = WinEventHandler()
+    strategy = BSPTilingStrategy()
+    window_manager = WindowManager(strategy)
+    event_handler = WinEventHandler()
 
-	app = wx.App()
-	taskbar = WimpyTaskBarIcon(PROGRAM_NAME, TRAY_ICON_PATH, window_manager)
-	event_handler.start_hook(window_manager.on_event, window_manager.on_error)
-	app.MainLoop()
-	event_handler.stop_hook()
-	sys.exit(0)
+    app = wx.App()
+    taskbar = WimpyTaskBarIcon(PROGRAM_NAME, TRAY_ICON_PATH, window_manager)
+    event_handler.start_hook(window_manager.on_event, window_manager.on_error)
+    app.MainLoop()
+    event_handler.stop_hook()
+    sys.exit(0)
+
 
 if __name__ == "__main__":
-	main()
+    main()
